165683a2e769d48baf7c3ae00db8b574bfe9328e
Rishikesh P C <hunterpckgh@gmai.com>
2023-05-16 16:20:59 +0530

    stopwatch start function finished

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 let seconds = document.querySelector("#seconds")
-let miliseconds = document.querySelector("#miliseconds")
+let miliSeconds = document.querySelector("#miliseconds")
+let minute = document.querySelector("#minute")
 let start = document.querySelector("#start")
 let stop = document.querySelector("#stop")
 let reset = document.querySelector("#reset")
 
 
 function updateTime() {
-    let time = 0
+    let secondsTime = 0
+    let miliSecondsTime = 0
+    minuteTime = 0
+
     setInterval(() => {
-        time++
-        seconds.innerHTML = time
+        secondsTime++
+        seconds.innerHTML = secondsTime
     }, 1000);
+
+    
+    setInterval(() => {
+        miliSecondsTime++
+        miliSeconds.innerHTML = miliSecondsTime
+    }, 100);
+
+    setInterval(() => {
+        minuteTime++
+        minute.innerHTML = minuteTime
+        secondsTime=0
+    }, 60000);
+    
 }
 
-
-
-start.addEventListener('click', ()=> {
-    updateTime()
-
-})  
+start.addEventListener('click', ()=> updateTime())  
